0bcc491b4e66f2285ad90de9e6cbf0590abddc20
rookieisme111 <1183914394@qq.com>
2021-09-09 16:41:51 +0800

    修改divide_valid.py，为traffic数据集划分

--- divide_valid.py
+++ divide_valid.py
@@ -1,25 +1,36 @@
 import json
 import random
 
-train_path = "./dataset/semeval/train.json"
-with open(train_path,'r') as f:
+file_path = "./dataset/gcn_dataset.json"
+with open(file_path,'r',encoding='utf-8') as f:
     trian_dict = json.load(f)
 #    print(trian_dict)
 
 print(len(trian_dict))
 
+samples_num = len(trian_dict)
+
 indeces = list(range(len(trian_dict)))
 random.shuffle(indeces)
 trian_dict=[ trian_dict[i] for i in indeces]
 
-new_train_path="./dataset/semeval/new_train.json"
-new_trian_list= trian_dict[:7200]
-print(len(new_trian_list))
-with open(new_train_path,'w') as f:
-    json.dump(new_trian_list,f)
+train_num = int(samples_num * 0.7)
+dev_num = int(samples_num * 0.1)
 
-new_dev_path="./dataset/semeval/new_dev.json"
-new_dev_list= trian_dict[7200:]
+new_train_path="./dataset/traffic/train.json"
+new_trian_list= trian_dict[:train_num]
+print(len(new_trian_list))
+with open(new_train_path,'w',encoding='utf-8') as f:
+    json.dump(new_trian_list,f,ensure_ascii=False)
+
+new_dev_path="./dataset/traffic/dev.json"
+new_dev_list= trian_dict[train_num:train_num+dev_num]
 print(len(new_dev_list))
-with open(new_dev_path,'w') as f:
-    json.dump(new_dev_list,f)
+with open(new_dev_path,'w',encoding='utf-8') as f:
+    json.dump(new_dev_list,f,ensure_ascii=False)
+
+new_test_path="./dataset/traffic/test.json"
+new_test_list= trian_dict[train_num+dev_num:]
+print(len(new_test_list))
+with open(new_test_path,'w',encoding='utf-8') as f:
+    json.dump(new_test_list,f,ensure_ascii=False)
